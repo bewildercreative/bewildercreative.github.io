75facd22b75dec42aeee29e35f9126d42d13322d
synchrohalo <joanne.b.arboleda@gmail.com>
2017-09-19 12:00:57 -0700

    lights reposition on window resize

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,11 @@ $( document ).ready( function() {
     $(".svg-container").css( "position", "fixed" );
 
     $( window ).scroll( function(){
-        $(".svg-container").css("opacity", 1 - $(window).scrollTop() / 300);
+        $( ".svg-container" ).css( "opacity", 1 - $( window ).scrollTop() / 800 );
+    });
+
+    $(window).resize( function(){
+        $( "#lights" ).load(window.location.href + " #lights" );
+        //location.reload();
     });
 });
