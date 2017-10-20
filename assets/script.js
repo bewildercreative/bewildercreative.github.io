9e55879753d9139190b10f9c738962f5fa12b3e4
synchrohalo <joanne.b.arboleda@gmail.com>
2017-10-19 19:11:30 -0700

    dark blue hero bg

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
 $( document ).ready( function() {
-    //$(".svg-container").css( "position", "fixed" );
-    //$(".svg-container").css( "background", "#00006c" );
-    //$("#hero").css( "color", "#f5f1f1" );
+    $(".svg-container").css( "position", "fixed" );
+    $("#hero").css( "background-color", "rgba( 0, 0, 118, 1 )" );
+    //$("#hero").css( "background-color", "linear-gradient( rgba( 0, 0, 118, 1 ), rgba( 224, 211, 209, 1 ) )" );
+    $("#hero").css( "color", "#f5f1f1" );
 
     $( window ).scroll( function(){
         //console.log(1 - $( window ).scrollTop() / 300);
         $( ".svg-container" ).css( "opacity", 1 - $( window ).scrollTop() / 300 );
 
-        /*if () {
+        $( "#hero" ).css( "background-color", "rgba( 0, 0, 118, " + ( 1 - $( window ).scrollTop() / 300 ) + " )" );
+
+        //console.log("rgba( 0, 0, 118, " + ( 1 - $( window ).scrollTop() / 300 ) + " )");
+
+        if ( ( 1 - $( window ).scrollTop() / 300 ) <= 0 ) {
           $( ".svg-container" ).hide();
-        }*/
+        } else {
+          $( ".svg-container" ).show();
+        }
     });
 
     $(window).resize( function(){
