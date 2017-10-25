77e7f983fe269dd453ad5dfdd7ca96c3a183e10e
synchrohalo <joanne.b.arboleda@gmail.com>
2017-10-25 11:20:17 -0700

    added a nav bar, disabled navy blue hero bg

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,48 @@
 $( document ).ready( function() {
-    $(".svg-container").css( "position", "fixed" );
-    $("#hero").css( "background-color", "rgba( 0, 0, 118, 1 )" );
-    $("#hero").css( "background-color", "linear-gradient( rgba( 0, 0, 118, 1 ), rgba( 224, 211, 209, 1 ) )" );
-    $("#hero").css( "color", "#f5f1f1" );
+  $(".svg-container").css( "position", "fixed" );
+  //$("#hero").css( "background-color", "rgba( 0, 0, 118, 1 )" );
+  //$("#hero").css( "background-color", "linear-gradient( rgba( 0, 0, 118, 1 ), rgba( 224, 211, 209, 1 ) )" );
+  //$("#hero").css( "color", "#f5f1f1" );
 
-    $( window ).scroll( function(){
-        //console.log(1 - $( window ).scrollTop() / 300);
-        $( ".svg-container" ).css( "opacity", 1 - $( window ).scrollTop() / 300 );
+  $( window ).scroll( function(){
+      //console.log(1 - $( window ).scrollTop() / 300);
+      $( ".svg-container" ).css( "opacity", 1 - $( window ).scrollTop() / 300 );
 
-        $( "#hero" ).css( "background-color", "rgba( 0, 0, 118, " + ( 1 - $( window ).scrollTop() / 300 ) + " )" );
+      //$( "#hero" ).css( "background-color", "rgba( 0, 0, 118, " + ( 1 - $( window ).scrollTop() / 300 ) + " )" );
 
-        //console.log("rgba( 0, 0, 118, " + ( 1 - $( window ).scrollTop() / 300 ) + " )");
+      //console.log("rgba( 0, 0, 118, " + ( 1 - $( window ).scrollTop() / 300 ) + " )");
 
-        if ( ( 1 - $( window ).scrollTop() / 300 ) <= 0 ) {
-          $( ".svg-container" ).hide();
-        } else {
-          $( ".svg-container" ).show();
-        }
-    });
+      if ( ( 1 - $( window ).scrollTop() / 200 ) <= 0 ) {
+        $( ".svg-container" ).hide();
+      } else {
+        $( ".svg-container" ).show();
+      }
+  });
 
-    $(window).resize( function(){
-        $( "#lights" ).load(window.location.href + " #lights" );
-        //location.reload();
-    });
+  $(window).resize( function(){
+      $( "#lights" ).load(window.location.href + " #lights" );
+      //location.reload();
+  });
+
+    // Select all links with hashes
+  $('a[href*="#"]').click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000 ); 
+      }
+    }
+  });
 });
